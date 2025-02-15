d4f26f5b9cc67110d8a891b316fb53a2a40442eb
Kamalesh <kamalesh2023@gmail.com>
2025-02-16 02:03:48 +0530

    removed unncecesary variable

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -28,7 +28,7 @@ export default function Signup() {
     };
 
     try {
-      const response = await axios.post(
+      await axios.post(
         "http://localhost:1337/api/auth/local/register",
         jsonData
       );
